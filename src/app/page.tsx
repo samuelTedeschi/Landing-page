a9d956d9d1c993b59390062cc4b2644e65cad1ab
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-08-01 12:22:22 -0300

    1.0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
+    <section className="mx-auto w-full">
       <Header />
 
-      <div className="h-screen bg-lime-950 flex justify-between">
-        <div className="flex flex-col space-y-60">
-          <h2 className="text-6xl text-gray-100 py-10 px-8">
+      <div className="h-screen bg-lime-950 flex items-center justify-around">
+        <div className="flex flex-col ">
+          <h2 className="text-7xl text-gray-100  p-8">
             Conheça
             <br /> nossa equipe
           </h2>
 
-          <div className="text-zinc-50 m-8 ">
+          <div className="text-zinc-50 p-8">
             <div className="flex space-x-10">
               <div>
                 <h3 className="text-xl">Joao Filipe</h3>
@@ -38,10 +38,10 @@ export default function Home() {
           </div>
         </div>
         <div className="grid gap-4 grid-cols-2 m-12">
-          <div className="bg-zinc-400 p-28 rounded-lg"></div>
-          <div className="bg-zinc-400 p-28 rounded-lg"></div>
-          <div className="bg-zinc-400 p-28 rounded-lg"></div>
-          <div className="bg-zinc-400 p-28 rounded-lg"></div>
+          <div className="bg-zinc-400 p-28"></div>
+          <div className="bg-zinc-400 p-28"></div>
+          <div className="bg-zinc-400 p-28"></div>
+          <div className="bg-zinc-400 p-28"></div>
         </div>
       </div>
 
@@ -49,9 +49,7 @@ export default function Home() {
         <div>
           <h2 className="text-6xl pt-10">Novos imoveis</h2>
         </div>
-        <div>
-
-        </div>
+        <div></div>
       </div>
 
       <div className="h-screen bg-zinc-950 text-white">
@@ -62,22 +60,81 @@ export default function Home() {
 
       <div className="h-screen bg-lime-950 text-white">
         <div>
-          <h2 className="text-6xl pt-10">Por que trabalhar <br/> conosco</h2>
+          <h2 className="text-6xl p-8">
+            Por que trabalhar <br /> conosco
+          </h2>
         </div>
       </div>
 
-      <div className="h-screen">
+      <div className="h-screen p-8">
         <div>
-          <h2 className="text-6xl pt-10">Depoimento</h2>
+          <h2 className="text-6xl">Depoimento</h2>
+        </div>
+
+        <div className="grid grid-cols-3 gap-8 pt-20">
+
+          <div className="p-6 border space-y-4">
+            <p className="leading-relaxed ">
+              Aumente a credibilidade do seu produto ou serviço inserindo
+              depoimentos dos seus clientes. Todo mundo adora recomendações,
+              então o depoimento de outros clientes é inestimável.
+            </p>
+            <p>- Jean e Juliana Medeiros</p>
+          </div>
+
+          <div className="p-6 border space-y-4">
+            <p className="leading-relaxed ">
+              Aumente a credibilidade do seu produto ou serviço inserindo
+              depoimentos dos seus clientes. Todo mundo adora recomendações,
+              então o depoimento de outros clientes é inestimável. Aumente a
+              credibilidade do seu produto ou serviço inserindo depoimentos dos
+              seus clientes.
+            </p>
+            <p>- Helena Pinheiro</p>
+          </div>
+          
+          <div className="p-6 border space-y-4">
+            <p className="leading-relaxed ">
+              Aumente a credibilidade do seu produto ou serviço inserindo
+              depoimentos dos seus clientes.
+            </p>
+            <p>- Roberto Gusmão</p>
+          </div>
         </div>
       </div>
 
-      <div className="h-screen bg-zinc-950 text-white">
+      <div className="h-screen w-full bg-zinc-950 text-white p-8">
         <div>
-          <h2 className="text-6xl pt-10">Tem duvidas <br/> Entre em Contato</h2>
+          <h2 className="text-6xl">
+            Tem duvidas <br /> Entre em Contato
+          </h2>
+        </div>
+
+        <div className="flex items-center space-x-4 pt-20">
+          <Image
+            src="/reuniao.jpg"
+            alt="reuniao"
+            width={700}
+            height={200}
+            className="grayscale"
+          />
+
+          <div className="space-y-8 p-4">
+            <div>
+              <p className="text-xl">Telefone</p>
+              <p>00 1234-5678</p>
+            </div>
+            <div>
+              <p className="text-xl">E-mail</p>
+              <p>negocio@email.com</p>
+            </div>
+            <div>
+              <p className="text-xl">Social</p>
+              <p>@social</p>
+            </div>
+          </div>
         </div>
       </div>
-  
     </section>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,24 @@
-import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="h-screen">
-      <div className="flex justify-between border-b border-b-zinc-900 m-8 pb-2">
+    <header className="h-screen mx-auto w-full p-8 ">
+      <div className="flex justify-between border-b border-b-zinc-900 pb-2">
         <p className="text-sm">Melhor imobiliaria do Brasil</p>
         <h2 className="font-bold text-lg">Grupo Philliper</h2>
         <div className="text-sm">Fale conosco</div>
       </div>
 
-      <div className="float-right mr-8">
-        <h1 className="text-6xl">
+      <div className="pt-7 space-y-8">
+        <h1 className="text-6xl font-bold">
           Quer comprar ou <br /> vender um imóvel?
         </h1>
-        <p className="mt-4 text-4xl">Voce veio ao lugar certo.</p>
+        <p className="text-4xl">Voce veio ao lugar certo.</p>
+        <div>
+          <div className="border p-48 bg-[url('https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg')] bg-cover bg-center grayscale "></div>
+        </div>
       </div>
 
-      <div className="m-8">
-        <Image
-          src="/"
-          alt="casa"
-          width={300}
-          height={100}
-          className="drop-shadow-lg grayscale object-fill w-[420px]"
-        />
-      </div>
+
     </header>
   );
 }
